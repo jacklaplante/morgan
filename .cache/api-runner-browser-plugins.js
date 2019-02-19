20c2887a05c902ce93666f27c54b6341ebf9e170
Jack LaPlante <jack.laplante@toasttab.com>
2019-02-19 16:33:11 -0500

    deleted a lot

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/jacklaplante/nonToast/morgan/node_modules/gatsby-plugin-emotion/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/jacklaplante/nonToast/morgan/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
-      options: {"plugins":[],"pathToConfigModule":"src/utils/typography.js"},
-    }]
